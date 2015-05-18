9882aa8567b7d649fbb6159c46a1ad5fb94c15af
ginetm <ginetm@127.0.0.1>
2015-05-18 14:53:04 +0200

    debut case

---
+++ PROJET/src/Case.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.util.*;
+public class Case {
+	private enum Etat {
+	joueur1,
+	joueur2,
+	vide;
+	}
+	
+	ArrayList<Point> succ;
+	Etat occupation;
+	Point pos;
+	
+	Case(Etat e, Point p) {
+		occupation = e;
+		pos = p;
+		succ = new ArrayList<Point>();
+		if(pos.x%2 != 0 && pos.y%2 != 0) {
+		
+		}
+	}
+	
+	Etat getOccupation() {
+		return occupation;
+	}
+	
+	void setOccupation(Etat e) {
+		occupation = e;
+	}
+}
