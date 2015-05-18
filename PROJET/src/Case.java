61e992c2845bfe9e3907551bc80a1b4eb9547d34
courbima <courbima@127.0.0.1>
2015-05-18 15:15:50 +0200

    Ajout de la méthode "initSuccesseurs" dans la classe Case

--- PROJET/src/Case.java
+++ PROJET/src/Case.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 
-public class Case {
-
-=======
 import java.awt.*;
 import java.util.*;
 public class Case {
@@ -32,5 +28,41 @@ public class Case {
 	void setOccupation(Etat e) {
 		occupation = e;
 	}
->>>>>>> branch 'master' of https://github.com/dimpolth/gameProject-PROG6.git
+	
+	void initSuccesseurs(){
+		this.succ = new ArrayList<Point>();
+		boolean diagonales = false;
+		
+		/* Dans tous les cas nous avons les successeurs haut,bas,droite,gauche
+		 * on créé donc si possible ces 4 successeurs
+		 */
+		if(this.pos.x - 1 > 0) // successeur haut
+			this.succ.add(new Point(this.pos.x - 1,this.pos.y));
+		if(this.pos.x + 1  < 5) // successeur bas
+			this.succ.add(new Point(this.pos.x + 1,this.pos.y));
+		if(this.pos.y - 1 > 0) // successeur gauche
+			this.succ.add(new Point(this.pos.x,this.pos.y - 1));
+		if(this.pos.y + 1 < 9) // successeur droite
+			this.succ.add(new Point(this.pos.x,this.pos.y + 1));
+		
+		if((this.pos.x % 2) == 0) // on est sur une ligne impaire (en admettant que la ligne 0 est en fait la première ligne)
+			if((this.pos.y % 2) == 0) // on est sur une colonne impaire (en admettant que la colonne 0 est en fait la première colonne)
+				diagonales = true;
+			
+		else  // on est sur une ligne paire
+			if((this.pos.y % 2) == 1) // on est sur une colonne paire
+				diagonales = true;
+		
+		if(diagonales){ // on doit ajouter SI POSSIBLE les 4 successeurs présents en diagonale
+			if( (this.pos.x - 1 > 0) && (this.pos.y - 1 > 0) ) // successeur diagonale haut/gauche
+				this.succ.add(new Point(this.pos.x - 1,this.pos.y - 1));
+			if( (this.pos.x - 1 > 0) && (this.pos.y + 1 < 9) ) // successeur diagonale haut/droite
+				this.succ.add(new Point(this.pos.x - 1,this.pos.y + 1));
+			if( (this.pos.x + 1 < 5) && (this.pos.y - 1 > 0) ) // successeur diagonale bas/gauche
+				this.succ.add(new Point(this.pos.x + 1,this.pos.y - 1));
+			if( (this.pos.x + 1 < 5) && (this.pos.y + 1 < 9) ) // successeur diagonale bas/droite
+				this.succ.add(new Point(this.pos.x + 1,this.pos.y + 1));
+		}
+		
+	}
 }
