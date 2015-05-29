9b7d7a4709a9df231f60987996453930c8101b4a
adpeyre <peyread@127.0.0.1>
2015-05-29 15:07:47 +0200

    changement alpha

--- PROJET/src/ihm/Pion.java
+++ PROJET/src/ihm/Pion.java
@@ -37,6 +37,10 @@ class Pion extends JComponent implements MouseListener, ComponentListener {
 	}
 	public void setCouleur(Case.Etat t) {
 		etat = t;
+		if(t == Case.Etat.vide)
+			alpha = 0.0f;
+		else
+			alpha = 1.0f;
 	}
 	public void setAlpha(float f) {
 		alpha = f;
