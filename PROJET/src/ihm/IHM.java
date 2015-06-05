54eab7f3ef96ea8873a78cb57daadcf62fa63ddc
peyread <peyread@127.0.0.1>
2015-06-05 10:45:50 +0200

    Correction problème reception paramètres ihm

--- PROJET/src/ihm/IHM.java
+++ PROJET/src/ihm/IHM.java
@@ -524,8 +524,10 @@ public class IHM extends JFrame implements ComponentListener {
 		}
 		if((dataValue = e.get("parametres")) != null) {
 			Parametres params = (Parametres)dataValue;
-			bandeauInfos.setIdentifiant(1,params.j1_identifiant);
-			bandeauInfos.setIdentifiant(2,params.j2_identifiant);			
+			if(params.j1_identifiant != null)
+				bandeauInfos.setIdentifiant(1,params.j1_identifiant);
+			if(params.j2_identifiant != null)
+				bandeauInfos.setIdentifiant(2,params.j2_identifiant);			
 		}
 		if((dataValue = e.get("finTour")) != null) {
 			boutonValidation.setEnabled((boolean)dataValue);
